8c388fa82b0abfb48b9f4aa9a880ff8dff34ed51
Chanhee <cgim971@gmail.com>
2023-03-13 10:32:38 +0900

    모니터 반영

--- ThreadTest/ThreadTest/Program.cs
+++ ThreadTest/ThreadTest/Program.cs
@@ -2,22 +2,31 @@
     internal class Program {
         static void Main(string[] args) {
             int num = 0;
+
+            object obj = new object();
+
             Thread t1 = new Thread(() => {
                 for (int i = 0; i < 1000000; i++) {
-                    num++;
-                    // A1 : 메모리에서 0을 가져오고
-                    // A2 : 1을 더하고
-                    // A3 : 메모리에 1을 저장
+                    try {
+                        Monitor.Enter(obj);
+                        num++;
+                    }
+                    finally {
+                        Monitor.Exit(obj);
+                    }
                 }
             });
             t1.Start();
 
             Thread t2 = new Thread(() => {
                 for (int i = 0; i < 1000000; i++) {
-                    num++;
-                    // B1 : 메모리에서 0을 가져오고
-                    // B2 : 1을 더하고
-                    // B3 : 메모리에 1을 저장
+                    try {
+                        Monitor.Enter(obj);
+                        num++;
+                    }
+                    finally {
+                        Monitor.Exit(obj);
+                    }
                 }
             });
             t2.Start();
